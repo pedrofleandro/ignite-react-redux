5eadaaae404986ebdb830a00c898a2ade795dd5e
pedrofleandro <pedrofragosoleandro@gmail.com>
2022-04-05 15:09:42 -0300

    Commit project

---
+++ src/store/modules/cart/actions.ts
@@ -0,0 +1,28 @@
+import { ActionsTypes, IProduct } from "./types";
+
+export function addProductToCartRequest(product: IProduct) {
+    return {
+        type: ActionsTypes.addProductToCartRequest,
+        payload: {
+            product,
+        }
+    }
+}
+
+export function addProductToCartSuccess(product: IProduct) {
+    return {
+        type: ActionsTypes.addProductToCartSuccess,
+        payload: {
+            product,
+        }
+    }
+}
+
+export function addProductToCartFailure(productId: number) {
+    return {
+        type: ActionsTypes.addProductToCartFailure,
+        payload: {
+            productId,
+        }
+    }
+}
---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,21 @@
+export enum ActionsTypes {
+    addProductToCartRequest = 'ADD_PRODUCT_TO_CART_REQUEST',
+    addProductToCartSuccess = 'ADD_PRODUCT_TO_CART_SUCCESS',
+    addProductToCartFailure = 'ADD_PRODUCT_TO_CART_FAILURE',
+}
+
+export interface IProduct {
+    id: number;
+    title: string;
+    price: number;
+}
+
+export interface IcartItem {
+    product: IProduct;
+    quantity: number;
+}
+
+export interface ICartState {
+    items: IcartItem[];
+    failedStockCheck: number[];
+}
